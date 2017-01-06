a4d8b58653c4dc9520c1540c7246a4b9e3b26a97
air <air@airdeMacBook-Air.local>
2017-01-06 15:16:13 +0800

    air modify

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 #dayby markdown blog system 
+
+### To Do
+
+- 将使用scp上传改为通过git push 然后 服务器端使用 git pull进行下载。将git pull写到一个PHP的功能里，转化完格式直接请求服务自动进行pull
--- bin/tohtml.py
+++ bin/tohtml.py
@@ -5,7 +5,8 @@ binpath='/Users/air/Documents/Dayby/bin'
 #from ./bin get previous path.The app root path.
 rootpath=binpath[:-4]
 mdpath=rootpath+'/md'#md files path
-htmlspath=rootpath+'/htmls'#write to htmls path
+#htmlspath=rootpath+'/htmls'#write to htmls path
+htmlspath=rootpath+'/blog/daily'
 Files=os.listdir(mdpath)
 mdfiles=[]
 for files in Files:
